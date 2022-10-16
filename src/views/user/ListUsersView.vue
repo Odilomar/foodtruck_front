323731fbbd7b6184594596558aa2810004e0babc
Odilomar Júnior <BRSSI0007@totvspartners.com.br>
2022-10-16 14:49:17 -0300

    feat: create list users view

--- src/views/user/ListUsersView.vue
+++ src/views/user/ListUsersView.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="container">
-    <h1>Todos os usuários</h1>
+    <div class="row">
+      <div class="col">
+        <h1>Todos os usuários</h1>
+      </div>
+      <div class="col-3">
+        <button type="button" class="btn btn-primary">Adicionar usuário</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">lista</div>
+    </div>
   </div>
 </template>
 
 <style></style>
+
+<script>
+import api from "../../api";
+
+export default {
+  async mounted() {
+    try {
+      const users = await api.users.getAllUsers();
+      console.log({ users });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
