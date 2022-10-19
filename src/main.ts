c09daa7c9df549569e5d1bce853d1a65c658489b
Odilomar Júnior <BRSSI0007@totvspartners.com.br>
2022-10-18 21:47:07 -0300

    feat: import bootstrap global

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import BootstrapVue from "bootstrap-vue-3";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(BootstrapVue);
 
 app.mount("#app");
