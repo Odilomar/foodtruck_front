1251207309307202d6ec664c22ec779eec420284
Odilomar Júnior <BRSSI0007@totvspartners.com.br>
2022-10-30 12:31:19 -0300

    fix: update home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import UserControllerView from "../views/user/UserControllerView.vue";
-import ProductControllerView from "../views/product/ProductControllerView.vue";
+import UserControllerView from "@/views/user/UserControllerView.vue";
+import ProductControllerView from "@/views/product/ProductControllerView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
+      name: "homeview",
+      component: HomeView,
+    },
+    {
+      path: "/products",
       name: "productcontrollerview",
       component: ProductControllerView,
     },
