b634bdb50e5b9275202e86a58795f3b1ede247a0
Odilomar Júnior <BRSSI0007@totvspartners.com.br>
2022-10-30 12:18:24 -0300

    fix: add padding for layout

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <div class="d-flex h-100 justify-content-center pt-4">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
